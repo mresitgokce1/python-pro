c6cc403106bf93742d69dfe13d3e15f836f3ded6
resit <mrgokce@yandex.com>
2025-01-15 17:56:39 +0000

    changed config

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or '97CU8jSU5d2a9eyNopZi3O7LNtGteeFB'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///exam_system.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-123'
+    if os.environ.get('PYTHONANYWHERE_DOMAIN'):
+        SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://mrgokce:Dsa13542o1o!@mrgokce.mysql.pythonanywhere-services.com/mrgokce$default'
+    else:
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///exam_system.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
